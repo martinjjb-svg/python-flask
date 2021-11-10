a40f0df50d7c5c6390823a73e406540216d6e1f6
Martin <martinjjb@gmail.com>
2021-11-10 15:38:11 +0000

    added GET and POST message

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+#these are GET functions
 @app.route("/")
 def hello_world():
     return "<p>This is a test for Hello, World!</p>"
+
+#GET function
+@app.route("/about/")
+def about():
+    return "<p>About the author</p>"
+
+#GET function
+@app.route("/square/<number>")
+def square(number):
+    return "<p>" + str(number) + "</p>"
+    #return "<p>The square of number is" + str(n) + "</p>"
+
+
+    #Post function
+@app.route("/users", methods=['POST'])
+def users():
+    data = request.form
+    first_name = data["first_name"]
+    last_name = data["last_name"]
+    return "<p>new_user_added" + first_name + last_name + "</p>"
