5cb35f1d94f93558429ed6a5aa4c727ec680a746
Martin <martinjjb@gmail.com>
2021-11-14 19:34:49 +0000

    Correcting code to run GET and POST

--- app.py
+++ app.py
@@ -2,27 +2,33 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-#these are GET functions
+#GET function: Runs the hello world on local host
 @app.route("/")
 def hello_world():
     return "<p>This is a test for Hello, World!</p>"
 
-#GET function
+#GET function: Adding a sub-level to the route of the URL
 @app.route("/about/")
 def about():
     return "<p>About the author</p>"
 
-#GET function
-@app.route("/square/<number>")
+#GET function: Taking in a parameter using the URL
+@app.route("/square/<number>", methods=['GET'])
 def square(number):
+    number = int(number)  # This is required to change type to Integer
+    number = number * number
     return "<p>" + str(number) + "</p>"
-    #return "<p>The square of number is" + str(n) + "</p>"
 
 
-    #Post function
+#Users Functionality
 @app.route("/users", methods=['POST'])
 def users():
-    data = request.form
-    first_name = data["first_name"]
-    last_name = data["last_name"]
-    return "<p>new_user_added" + first_name + last_name + "</p>"
+
+    #POST method for
+    if request.method == 'POST':
+        first_name = request.json["first_name"]
+        last_name = request.json["last_name"]
+        return "<p>new_user_added" + first_name + " " + last_name + "</p>"
+
+if __name__ == '__main__':
+    app.run()
